abf933d866762147116a52f3cfea0caee1412a24
VikashhAdaikalavan <71553967+VikashhAdaikalavan@users.noreply.github.com>
2024-08-31 16:40:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask, render_template, request, redirect, url_for
+import pickle
 
 app = Flask(__name__,template_folder = 'templates' )
 
 
 todos=[]
+
+try:
+    f = open('tasks.dat','rb')
+    while True:
+        a = pickle.load(f)
+        todos.append(a)
+    f.close()
+except:
+    pass
 
 
 @app.route("/")
@@ -15,6 +25,9 @@ def index():
 def add():
     todo = request.form['todo']
     todos.append({'task':todo, 'done': False})
+    f = open('tasks.dat','ab')
+    pickle.dump({'task':todo, 'done': False},f)
+    f.close()
     return redirect(url_for('index'))
 
 
@@ -23,6 +36,11 @@ def edit(index):
     todo = todos[index]
     if request.method == 'POST':
         todo['task']= request.form["todo"]
+        f = open("tasks.dat",'wb')
+        for i in todos:
+            pickle.dump(i,f)
+        f.close()
+
         return redirect(url_for('index'))
     else:
         return render_template("edit.html",todo = todo, index = index)
@@ -30,11 +48,20 @@ def edit(index):
 @app.route("/check/<int:index>")
 def check(index):
     todos[index]['done']= not todos[index]['done']
+    f = open("tasks.dat",'wb')
+    for i in todos:
+        pickle.dump(i,f)
+    f.close()
+
     return redirect(url_for('index'))
 
 @app.route("/delete/<int:index>")
 def delete(index):
+    f = open("tasks.dat",'wb')
     del todos[index]
+    for i in todos:
+        pickle.dump(i,f)
+    f.close()
     return redirect(url_for('index'))
 
 
